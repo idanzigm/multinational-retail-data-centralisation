36691baf79b2f321afb719537007ad74b52169b7
abimcph <abimcph@yahoo.com>
2023-11-03 13:11:38 +0000

    Removed read_db_creds.py and added information to database_utils.py class.

--- read_db_creds.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-class DatabaseConnector:
-
-    def read_db_creds(self):
-        with open('db_creds.yaml', 'r') as file:
-            creds = yaml.safe_load(file)
-        return creds
